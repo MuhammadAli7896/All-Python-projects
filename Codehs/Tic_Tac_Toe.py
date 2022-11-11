e4b89953ce0e394a9d87299f06242893a3fc9322
muhammadali7896 <muhammadali30804@gmail.com>
2022-11-12 00:45:07 +0500

    Update Tic_Tac_Toe.py

--- Codehs/Tic_Tac_Toe.py
+++ Codehs/Tic_Tac_Toe.py
@@ -1,10 +1,3 @@
-"""
-This program simulates the game of tic tac toe.
-"""
-
-# get_valid_index
-# -----
-# Get row or column from user
 def get_valid_index(prompt):
     while True:
         try:
@@ -14,6 +7,7 @@ def get_valid_index(prompt):
             print("Must be 0 - 2 inclusive!")
         except ValueError:
             print("Must be an integer!")
+    return
 
 # game_is_over
 # -----
@@ -58,30 +52,103 @@ def game_is_over(board):
     
     # Not over yet!
     return False
+
+def print_board():
+    one = 'X' if xstate[0] else ('O' if zstate[0] else 1)
+    two = 'X' if xstate[1] else ('O' if zstate[1] else 2)
+    three = 'X' if xstate[2] else ('O' if zstate[2] else 3)
+    four = 'X' if xstate[3] else ('O' if zstate[3] else 4)
+    five = 'X' if xstate[4] else ('O' if zstate[4] else 5)
+    six = 'X' if xstate[5] else ('O' if zstate[5] else 6)
+    seven = 'X' if xstate[6] else ('O' if zstate[6] else 7)
+    eight = 'X' if xstate[7] else ('O' if zstate[7] else 8)
+    nine = 'X' if xstate[8] else ('O' if zstate[8] else 9)
+    # if xstate[0]: # for one
+    #     one = 'X'
+    # elif zstate[0]:
+    #     one = 'O'
+    # else:
+    #     one= 1
+    # if xstate[1]: # for two
+    #     two = 'X'
+    # elif zstate[1]:
+    #     two = 'O'
+    # else:
+    #     two = 2
+    # if xstate[2]: # for three
+    #     three = 'X'
+    # elif zstate[2]:
+    #     three = 'O'
+    # else:
+    #     three = 3
+    # if xstate[3]: # for four
+    #     four = 'X'
+    # elif zstate[3]:
+    #     four = 'O'
+    # else:
+    #     four = 4
+    # if xstate[4]: # for five
+    #     five = 'X'
+    # elif zstate[4]:
+    #     five = 'O'
+    # else:
+    #     five = 5
+    # if xstate[5]: # for six
+    #     six = 'X'
+    # elif zstate[5]:
+    #     six = 'O'
+    # else:
+    #     six = 6
+    # if xstate[6]: #for seven
+    #     seven = 'X'
+    # elif zstate[6]:
+    #     seven = 'O'
+    # else:
+    #     seven = 7
+    # if xstate[7]: # for eight
+    #     eight = 'X'
+    # elif zstate[7]:
+    #     eight = 'O'
+    # else:
+    #     eight = 8
+    # if xstate[8]: # for nine
+    #     nine = 'X'
+    # elif zstate[8]:
+    #     nine = 'O'
+    # else:
+    #     nine = 9
+    print(f"{one} | {two} | {three} ")
+    print(f"--|---|---")
+    print(f"{four} | {five} | {six} ")
+    print(f"--|---|---")
+    print(f"{seven} | {eight} | {nine} ") 
     
-# print_board
-# -----
-# Print the board.
-def print_board(board):
-    # TODO: Replace the line below with your code...
-    print()
-
-# Set up board
-board = []
-# TODO: Set up the board as a 3x3 grid of spaces here...
-for i in range(3):
-    board.append([' '] * 3)
-for i in range(len(board)):
-    print(board[i])
-
-# x goes first
-turn = "x"
-
-# Play tic tac toe
-while not game_is_over(board):
-    print_board(board)
-    print("It's " + turn + "'s turn.")
-    row = get_valid_index("Row: ")
-    col = get_valid_index("Col: ")
-    
-    # TODO: Your code here...
+xstate = [0,0,0,0,0,0,0,0,0]
+zstate = [0,0,0,0,0,0,0,0,0]
+turn = 0
+print_board()
+while True:
+    #turn = 0
+    #print_board()
+    if turn % 2 == 0:
+        print("It's 'X' turn.")
+        X_turn = int(input("Enter a number (1-9): "))
+        turn += 1
+        if xstate[X_turn - 1] == 'O':
+            print("Enter another value.")
+            continue
+        else:
+            xstate[X_turn - 1] = True
+            print_board()
+    elif turn %2 == 1:
+        print("It's 'O' turn.")
+        Z_turn = int(input("Enter a value (1-9): "))
+        turn += 1
+        if zstate[Z_turn - 1] == 'X':
+            print("Enter another value.")
+            continue
+        else:
+            zstate[Z_turn - 1] = True
+            print_board()
+    else:
+        continue
