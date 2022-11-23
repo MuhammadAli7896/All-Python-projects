4d99e8476ca464d1d88a0541b841121ccc97548a
muhammadali7896 <muhammadali30804@gmail.com>
2022-11-23 23:03:11 +0500

    Update Tic_Tac_Toe.py

--- Codehs/Tic_Tac_Toe.py
+++ Codehs/Tic_Tac_Toe.py
@@ -1,3 +1,6 @@
+import colorama
+from colorama import Back,Fore,Style
+
 def get_valid_index(prompt):
     while True:
         try:
@@ -54,74 +57,22 @@ def game_is_over(board):
     return False
 
 def print_board():
-    one = 'X' if xstate[0] else ('O' if zstate[0] else 1)
-    two = 'X' if xstate[1] else ('O' if zstate[1] else 2)
-    three = 'X' if xstate[2] else ('O' if zstate[2] else 3)
-    four = 'X' if xstate[3] else ('O' if zstate[3] else 4)
-    five = 'X' if xstate[4] else ('O' if zstate[4] else 5)
-    six = 'X' if xstate[5] else ('O' if zstate[5] else 6)
-    seven = 'X' if xstate[6] else ('O' if zstate[6] else 7)
-    eight = 'X' if xstate[7] else ('O' if zstate[7] else 8)
-    nine = 'X' if xstate[8] else ('O' if zstate[8] else 9)
-    # if xstate[0]: # for one
-    #     one = 'X'
-    # elif zstate[0]:
-    #     one = 'O'
-    # else:
-    #     one= 1
-    # if xstate[1]: # for two
-    #     two = 'X'
-    # elif zstate[1]:
-    #     two = 'O'
-    # else:
-    #     two = 2
-    # if xstate[2]: # for three
-    #     three = 'X'
-    # elif zstate[2]:
-    #     three = 'O'
-    # else:
-    #     three = 3
-    # if xstate[3]: # for four
-    #     four = 'X'
-    # elif zstate[3]:
-    #     four = 'O'
-    # else:
-    #     four = 4
-    # if xstate[4]: # for five
-    #     five = 'X'
-    # elif zstate[4]:
-    #     five = 'O'
-    # else:
-    #     five = 5
-    # if xstate[5]: # for six
-    #     six = 'X'
-    # elif zstate[5]:
-    #     six = 'O'
-    # else:
-    #     six = 6
-    # if xstate[6]: #for seven
-    #     seven = 'X'
-    # elif zstate[6]:
-    #     seven = 'O'
-    # else:
-    #     seven = 7
-    # if xstate[7]: # for eight
-    #     eight = 'X'
-    # elif zstate[7]:
-    #     eight = 'O'
-    # else:
-    #     eight = 8
-    # if xstate[8]: # for nine
-    #     nine = 'X'
-    # elif zstate[8]:
-    #     nine = 'O'
-    # else:
-    #     nine = 9
+    one = f'{Fore.LIGHTRED_EX}X{Fore.RESET}' if xstate[0] else (f'{Fore.LIGHTGREEN_EX}O{Fore.RESET}' if zstate[0] else 1)
+    two = f'{Fore.LIGHTRED_EX}X{Fore.RESET}' if xstate[1] else (f'{Fore.LIGHTGREEN_EX}O{Fore.RESET}' if zstate[1] else 2)
+    three = f'{Fore.LIGHTRED_EX}X{Fore.RESET}' if xstate[2] else (f'{Fore.LIGHTGREEN_EX}O{Fore.RESET}' if zstate[2] else 3)
+    four = f'{Fore.LIGHTRED_EX}X{Fore.RESET}' if xstate[3] else (f'{Fore.LIGHTGREEN_EX}O{Fore.RESET}' if zstate[3] else 4)
+    five = f'{Fore.LIGHTRED_EX}X{Fore.RESET}' if xstate[4] else (f'{Fore.LIGHTGREEN_EX}O{Fore.RESET}' if zstate[4] else 5)
+    six = f'{Fore.LIGHTRED_EX}X{Fore.RESET}' if xstate[5] else (f'{Fore.LIGHTGREEN_EX}O{Fore.RESET}' if zstate[5] else 6)
+    seven = f'{Fore.LIGHTRED_EX}X{Fore.RESET}' if xstate[6] else (f'{Fore.LIGHTGREEN_EX}O{Fore.RESET}' if zstate[6] else 7)
+    eight = f'{Fore.LIGHTRED_EX}X{Fore.RESET}' if xstate[7] else (f'{Fore.LIGHTGREEN_EX}O{Fore.RESET}' if zstate[7] else 8)
+    nine = f'{Fore.LIGHTRED_EX}X{Fore.RESET}' if xstate[8] else (f'{Fore.LIGHTGREEN_EX}O{Fore.RESET}' if zstate[8] else 9)
+    print(Fore.LIGHTCYAN_EX)
     print(f"{one} | {two} | {three} ")
     print(f"--|---|---")
     print(f"{four} | {five} | {six} ")
     print(f"--|---|---")
     print(f"{seven} | {eight} | {nine} ") 
+    print(Fore.RESET)
 
 def sum(a, b, c):
     return a + b + c
@@ -129,7 +80,7 @@ def sum(a, b, c):
 xstate = [0,0,0,0,0,0,0,0,0]
 zstate = [0,0,0,0,0,0,0,0,0]
 
-def check_wins():
+def check_wins(xstate,zstate):
     win = [[1,2,3],[1,4,7],[2,5,8],[3,6,9],[4,5,6],[7,8,9],[1,5,9],[3,5,7]]
     for item in win:
         if xstate[item[0]-1] == True and xstate[item[1]-1] == True and xstate[item[2]-1] == True:
@@ -148,7 +99,7 @@ while True:
     #turn = 0
     #print_board()
     if turn % 2 == 0:
-        print("It's 'X' turn.")
+        print(f"{Fore.LIGHTRED_EX}It's 'X' turn.{Fore.RESET}")
         X_turn = int(input("Enter a number (1-9): "))
         if zstate[X_turn - 1] == True or xstate[X_turn - 1] == True:
             print("Enter another value.")
@@ -157,12 +108,12 @@ while True:
             xstate[X_turn - 1] = True
             print_board()
             turn += 1
-            who_win = check_wins()
+            who_win = check_wins(xstate,zstate)
             if who_win != -1:
                 print("Match over.")
                 break
     elif turn %2 == 1:
-        print("It's 'O' turn.")
+        print(f"{Fore.LIGHTGREEN_EX}It's 'O' turn.{Fore.RESET}")
         Z_turn = int(input("Enter a value (1-9): "))
         if xstate[Z_turn - 1] == True or zstate[Z_turn - 1] == True:
             print("Enter another value.")
@@ -171,7 +122,7 @@ while True:
             zstate[Z_turn - 1] = True
             print_board()
             turn += 1
-            who_win = check_wins()
+            who_win = check_wins(xstate,zstate)
             if who_win != -1:
                 print("Match over.")
                 break
