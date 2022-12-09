a9f6abbd5ba37a2e7b474012d89714014c90ad6a
muhammadali7896 <muhammadali30804@gmail.com>
2022-12-10 01:34:59 +0500

    Coding challenge question there is one query

---
+++ Coding_challenge_Question_2.py
@@ -0,0 +1,7 @@
+alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+n = int(input())
+for i in range(n):
+    for i in range(n):
+        print(alpha[i])
+    for i in range(n-1,0):
+        print(alpha[j])
