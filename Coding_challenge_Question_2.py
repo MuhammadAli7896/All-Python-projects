1f65be48e3bb2e58090aede47aac2743222f5930
muhammadali7896 <muhammadali30804@gmail.com>
2022-12-12 00:02:22 +0500

    Name modified of folder

--- Coding_challenge_Question_2.py
+++ Coding_challenge_Question_2.py
@@ -2,6 +2,6 @@ alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R'
 n = int(input())
 for i in range(n):
     for i in range(n):
-        print(alpha[i])
+        print(r'alpha[i]')
     for i in range(n-1,0):
         print(alpha[j])
