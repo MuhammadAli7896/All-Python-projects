ec1811634ce34330916d7a677d5dd601ff409c7e
muhammadali7896 <muhammadali30804@gmail.com>
2022-12-22 23:24:15 +0500

    Update Guess_The_Word_Game.py

--- Guess_The_Word_Game.py
+++ Guess_The_Word_Game.py
@@ -35,7 +35,7 @@ def get_guess():
             current_dashes_state = update_dashes(secret_word2, current_dashes_state, guess)
             print(current_dashes_state)
             if current_dashes_state == secret_word2:
-                print(f"Congrats! You win. The word was: {current_dashes_state}")
+                print(f"Congrats! You win.")
                 #break
                 break
             print(f"{guesses_left} incorrect guesses left.")
